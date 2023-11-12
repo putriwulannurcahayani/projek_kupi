b6a6302027c9c5e353ca35a5e9b41af0e381c9a6
putriwulannurcahayani <137497891+putriwulannurcahayani@users.noreply.github.com>
2023-11-12 22:14:29 +0700

    first commit

---
+++ resources/views/auth/signup.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Sign Up form </title> 
+    <link rel="stylesheet" href="style.css">
+   </head>
+<body>
+  <div class="wrapper">
+    <h2>Sign Up</h2>
+    <h6>Andy enjoy life during the time you just saved</h6>
+    <form action="#">
+      <div class="input-box"> 
+        <input type="text" placeholder="Nama" required>
+      </div>
+      <div class="input-box">
+        <input type="text" placeholder="Nama Usaha" required>
+      </div>
+      <div class="input-box">
+        <input type="text" placeholder="Email" required>
+      </div>
+      <div class="input-box">
+        <input type="password" placeholder="Password" required>
+      </div> 
+      <div class="input-box">
+        <input type="password" placeholder="Confirm Password" required>
+      </div>
+      <div class="policy">
+        <input type="checkbox">
+        <h3>I accept all terms & condition</h3>
+      </div>
+      <div class="input-box button">
+        <input type="Submit" value="Create on Acoount">
+      </div>
+      <div class="text">
+        <h3>Already have an account? <a href="#">Login now</a></h3>
+      </div>
+    </form>
+  </div>
+</body>
+</html>
